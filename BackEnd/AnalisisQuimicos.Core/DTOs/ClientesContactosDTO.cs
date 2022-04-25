e21b39fe93c871cbb0d1744649a757a9df0f385f
polyucode <83753042+polyucode@users.noreply.github.com>
2022-04-25 11:54:40 +0200

    added ClientesContactos to Back

---
+++ BackEnd/AnalisisQuimicos.Core/DTOs/ClientesContactosDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalisisQuimicos.Core.DTOs
+{
+    public class ClientesContactosDTO
+    {
+        public int? Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono1 { get; set; }
+        public string Extension { get; set; }
+        public string Telefono2 { get; set; }
+        public string Cargo { get; set; }
+        public string Email { get; set; }
+        public int? IdCliente { get; set; }
+        public DateTime? AddDate { get; set; }
+        public int? AddIdUser { get; set; }
+        public DateTime? ModDate { get; set; }
+        public int? ModIdUser { get; set; }
+        public DateTime? DelDate { get; set; }
+        public int? DelIdUser { get; set; }
+        public bool? Deleted { get; set; }
+    }
+}
---
+++ BackEnd/AnalisisQuimicos.Core/Entities/ClientesContactos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalisisQuimicos.Core.Entities
+{
+    public partial class ClientesContactos : BaseEntity
+    {
+            public string Nombre { get; set; }
+            public string Telefono1 { get; set; }
+            public string Extension { get; set; }
+            public string Telefono2 { get; set; }
+            public string Cargo { get; set; }
+            public string Email { get; set; }
+            public int? IdCliente { get; set; }
+
+    }
+}
