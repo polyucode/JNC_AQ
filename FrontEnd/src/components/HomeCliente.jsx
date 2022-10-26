3561a1b70c45541198ea78cadb0f9bf53e51d2fb
Aitor Subarroca <101728174+asubarroca@users.noreply.github.com>
2022-10-26 19:51:17 +0200

    Diagrama. Cargado en dashboard

--- FrontEnd/src/components/HomeCliente.jsx
+++ FrontEnd/src/components/HomeCliente.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from "axios";
 //import Diagram, { useSchema, createSchema } from 'beautiful-react-diagrams';
-import { FormControl, Grid, Card, CardActions, CardContent, CardMedia, Button, TextField, Typography, Alert, AlertTitle } from '@mui/material';
+import { FormControl, Grid, Card, CardActions, CardContent, CardMedia, Button, TextField, Typography, Alert, AlertTitle, Autocomplete } from '@mui/material';
 
 import {
     Chart,
@@ -15,7 +15,10 @@ import "hammerjs";
 
 import '@progress/kendo-theme-default/dist/all.css';
 import { Diagrama } from './Diagrama/Diagrama';
-import { getOfertas } from '../api/apiBackend';
+import { getConfPlantaClientePorClienteOferta, getOfertas } from '../api/apiBackend';
+import { AuthContext } from '../context/AuthContext';
+import { useDiagrama } from '../helpers/generarDiagrama';
+import ReactFlow, { Background } from 'react-flow-renderer';
 
 const token = {
     headers: {
@@ -28,7 +31,15 @@ const token = {
 const HomeCliente = () => {
 
     const [ valores, setValores ] = useState([]);
-    const [ ofertas, setOfertas ] = useState();
+    const [ ofertas, setOfertas ] = useState([]);
+    const [ plantaActiva, setPlantaActiva ] = useState({});
+    const [ nodos, setNodos ] = useState();
+    const [ lados, setLados ] = useState();
+
+    const { user } = useContext( AuthContext );
+    const { nodeTypes } = useDiagrama();
+
+
 
     const getValorParametros = async () => {
         axios.get("/valorparametros", token).then(response => {
@@ -46,6 +57,19 @@ const HomeCliente = () => {
 
     }, [])
 
+    // Efecto que carga el diagrama cada vez que se cambi de planta
+    useEffect(() => {
+
+        if( plantaActiva.diagrama ) {
+
+            const datosDiagrama = JSON.parse( plantaActiva.diagrama );
+            console.log( datosDiagrama );
+
+            setNodos( datosDiagrama.nodos );
+            setLados( datosDiagrama.lados );
+        }
+
+    }, [ plantaActiva ])
 
     
     const categories = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul","Ago","Sep","Oct","Nov","Dic"];
@@ -67,23 +91,72 @@ const HomeCliente = () => {
         </Chart>
       );
 
+    // Con esta función, al seleccionar una oferta seteamos la planta activa
+    const handleSeleccionOferta = (e) => {
+
+        const ofertaSeleccionada = parseInt(e.target.textContent);
+
+        getConfPlantaClientePorClienteOferta( user.idCliente, ofertaSeleccionada )
+            .then( res => setPlantaActiva( res ) );
+
+    }
 
     return (
         <>
             <Grid container spacing={ 2 }>
 
+                {/* APARTADO INICIAL, SELECCIÓN DE PLANTA */}
                 <Grid item xs={ 12 }>
                     <Card>
                         <CardContent>
-                            <Typography variant="h6">Info de la planta</Typography>
+                            <Grid container sx={{ alignItems: 'center' }}>
+
+                                <Grid item xs={ 6 }>
+                                    <Typography variant="h6">
+                                        {
+                                            plantaActiva.nombreCliente
+                                             ? plantaActiva.nombreCliente
+                                             : 'Selecciona un código de oferta'
+                                        }
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={ 4 }> 
+                                    {
+                                        plantaActiva.descripcion && (
+                                            <Typography>{ plantaActiva.descripcion }</Typography>
+                                        ) 
+                                    }
+                                </Grid>
+                                <Grid item xs={ 2 }>
+                                    <Autocomplete
+                                        disableClearable={ true }
+                                        id="ofertas"
+                                        options={ ofertas }
+                                        getOptionLabel={ option => option.numeroOferta.toString() }
+                                        renderInput={ params => <TextField {...params} label="Código oferta" name="codigoOferta" /> }
+                                        onChange={ handleSeleccionOferta }
+                                    />
+                                </Grid>
+
+                            </Grid>
                         </CardContent>
                     </Card>
                 </Grid>
 
+                {/* APARTADO DEL DIAGRAMA */}
                 <Grid item xs={ 12 }>
                     <Card>
                         <CardContent sx={{ p: 0, height: 400 }}>
-                            <Diagrama />
+                            <ReactFlow
+                                nodes={ nodos }
+                                edges={ lados }
+                                //onEdgesChange={onEdgesChange}
+                                //onConnect={onConnect}
+                                nodeTypes={ nodeTypes }
+                                fitView
+                            >
+                                <Background />
+                            </ReactFlow>
                         </CardContent>
                     </Card>
                 </Grid>
